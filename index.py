3e843ab8b3b757d0995e0f5f5bbbfa3ce9cf3e93
JonaThanosSnap <59585116+JonaThanosSnap@users.noreply.github.com>
2021-06-26 13:18:46 -0400

    added comments

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import csv
 import tkinter as tk
 
 window = tk.Tk()
-
+window.geometry("800x500")
+window.configure(bg = "")
 def search():
     global ingredients
     ingredients = text_box.get("1.0",tk.END).splitlines()
@@ -16,7 +17,6 @@ label.pack()
 text_box.pack()
 button.pack()
 window.mainloop()
-print(ingredients)
 
 makeable_food = []
 
@@ -36,12 +36,15 @@ with open('recipes_data.csv') as csv_file:
             if (matches/len(ingredient_list)) >= 0.8:
                 makeable_food.append(row[0]+" is rated "+row[1]+", has a difficulty rating of "+row[2]+", is a "+row[3]+", and takes "+row[4]+"m to make. The ingredients needed are: "+row[5]+"." )
 
+text_box2 = tk.Text(width = 200)
+text_box2.tag_configure("center",justify = "center")
 if len(makeable_food) > 0:
-    text_box2 = tk.Text(width = 200)
+    text_box2.insert("1.0","You can make the following dishes with your ingredients:" + "\n" + "\n")
     for x in makeable_food:
-        text_box2.insert(tk.END,"You can make the following dishes with your ingredients:" + "\n" + "\n" + x + "\n" + "\n")
+        text_box2.insert(tk.END, x + "\n" + "\n")
 
 else:
-    text_box2.insert("Unfortunately, nothing can be made with your ingredients")
+    text_box2.insert("5.0","Unfortunately, nothing can be made with your ingredients")
+text_box2.tag_add("center","1.0","end")
 text_box2.pack()
 window.mainloop()
