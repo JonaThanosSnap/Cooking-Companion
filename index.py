e03f17babe37e61d0d516ea9e4c0b1a750e706b9
JonaThanosSnap <59585116+JonaThanosSnap@users.noreply.github.com>
2021-06-26 11:33:42 -0400

    Reads a prints ingredients into arrays

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ def search():
 
 label = tk.Label(text="What ingredients do you own?")
 text_box = tk.Text()
-button = tk.Button(text="Search",command = search)
+button = tk.Button(text="Enter",command = search)
 
 label.pack()
 text_box.pack()
@@ -18,17 +18,21 @@ button.pack()
 window.mainloop()
 print(ingredients)
 
-#if sort_method == "rating":
- #   stars = int(input("How many stars would you like your dish to be? (0 - 5) "))
-  #  with open('recipes_data.csv') as csv_file:
-   #     csv_reader = csv.reader(csv_file, delimiter=',')
-    #    line_count = 0
-     #   for row in csv_reader:
-      #      if line_count == 0:
-       #         line_count += 1
-        #    else:
-                #print(f'\t{row[0]} has a difficulty of {row[1]},{row[2]}.')
-                #print({row[4]})
+with open('recipes_data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            print(row[5].split(","))
+            if ingredients[0] == row[5]:
+                print("JJJJJJJJJ")
+
+
+
+
+
          #       if str(stars*"⭐")==str({row[1]}):
           #          print({row[0]}+" has "+stars+" stars, is "+{row[2]}+" to make. It takes "+{row[4]}+" minutes to make and requires the following ingredients: "+{row[5]})
            #     line_count += 1
