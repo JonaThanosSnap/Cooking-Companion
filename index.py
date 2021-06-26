789581e8000efbc930625eba7229fa94eaf3360b
JonaThanosSnap <59585116+JonaThanosSnap@users.noreply.github.com>
2021-06-26 01:26:25 -0400

    editted the csv file

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-#hi
+import csv
+
+with open('recipes_data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            #print(f'\t{row[0]} has a difficulty of {row[1]},{row[2]}.')
+            print({row[4]})
+            line_count += 1
+    print(f'Processed {line_count} lines.')
